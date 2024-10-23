59b9e18963519d877a44f969ecf0cf207f4bebbd
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-23 16:13:58 +0530

    chore: update runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,6 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  runtimeConfig: {
-    public: {
-      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋',
-    },
-  },
-
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-07-30',
 
