b4c126997d68d863631fcf7d585bbaf40dbc5a07
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-23 19:09:04 +0530

    feat: add app colors config

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,18 @@
   <NuxtLoadingIndicator />
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  bodyAttrs: {
+    class: 'bg-gray-50 dark:bg-gray-950',
+  },
+});
+</script>
