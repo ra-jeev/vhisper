c034f9c77f1dc974bf8aa534c91c07f1a2d2905f
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-26 19:02:35 +0530

    feat: add drizzle config and user schema

---
+++ server/database/schema.ts
@@ -0,0 +1,16 @@
+import crypto from 'node:crypto';
+import { sql } from 'drizzle-orm';
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomBytes(12).toString('hex')),
+  name: text('name').notNull(),
+  username: text('username').notNull().unique(),
+  password: text('password').notNull(),
+  avatar: text('avatar'),
+  createdAt: integer('created_at', { mode: 'timestamp' }).default(
+    sql`(CURRENT_TIMESTAMP)`
+  ),
+});
