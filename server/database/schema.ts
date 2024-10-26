ecc4cdf7868988e5fbde224bb2669711b2e2ee52
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-26 22:43:04 +0530

    fix: correct date type in users schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,6 +1,6 @@
 import crypto from 'node:crypto';
 import { sql } from 'drizzle-orm';
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema } from 'drizzle-zod';
 
 export const users = sqliteTable('users', {
@@ -11,9 +11,7 @@ export const users = sqliteTable('users', {
   username: text('username').notNull().unique(),
   password: text('password').notNull(),
   avatar: text('avatar'),
-  createdAt: integer('created_at', { mode: 'timestamp' }).default(
-    sql`(CURRENT_TIMESTAMP)`
-  ),
+  createdAt: text('created_at').default(sql`(CURRENT_TIMESTAMP)`),
 });
 
 export const insertUserSchema = createInsertSchema(users);
