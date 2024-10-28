167534e20a78b071d85f4f2763a6bee3908fd51b
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-28 22:47:25 +0530

    feat: create userSchemas with drizzle-zod

--- types/index.ts
+++ types/index.ts
@@ -1,29 +1,67 @@
-import { z } from 'zod';
+import { createInsertSchema } from 'drizzle-zod';
+import type { z } from 'zod';
+import { users } from '~~/server/database/schema';
 
-export const authSchema = z.object({
-  username: z
-    .string()
-    .min(3, 'Username must be at least 3 characters long')
-    .max(20, 'Username cannot exceed 20 characters')
-    .regex(
-      /^[a-zA-Z0-9_]+$/,
-      'Username can only contain letters, numbers, and underscores'
-    ),
-  password: z
-    .string()
-    .min(8, 'Password must be at least 8 characters long')
-    .max(128, 'Password cannot exceed 128 characters'),
+const userValidationRules = {
+  username: {
+    min: 3,
+    max: 20,
+    pattern: /^[a-zA-Z0-9_]+$/,
+  },
+  password: {
+    min: 8,
+    max: 128,
+  },
+  name: {
+    min: 2,
+    max: 50,
+    pattern: /^[a-zA-Z\s]+$/,
+  },
+} as const;
+
+export const signUpSchema = createInsertSchema(users, {
+  name: (schema) =>
+    schema.name
+      .min(
+        userValidationRules.name.min,
+        'Name must be at least 2 characters long'
+      )
+      .max(userValidationRules.name.max, 'Name cannot exceed 50 characters')
+      .regex(
+        userValidationRules.name.pattern,
+        'Name can only contain letters and spaces'
+      ),
+  username: (schema) =>
+    schema.username
+      .min(
+        userValidationRules.username.min,
+        'Username must be at least 3 characters long'
+      )
+      .max(
+        userValidationRules.username.max,
+        'Username cannot exceed 20 characters'
+      )
+      .regex(
+        userValidationRules.username.pattern,
+        'Username can only contain letters, numbers, and underscores'
+      ),
+  password: (schema) =>
+    schema.password
+      .min(
+        userValidationRules.password.min,
+        'Password must be at least 8 characters long'
+      )
+      .max(
+        userValidationRules.password.max,
+        'Password cannot exceed 128 characters'
+      ),
+}).pick({
+  name: true,
+  username: true,
+  password: true,
 });
 
-export const signUpSchema = z
-  .object({
-    name: z
-      .string()
-      .min(2, 'Name must be at least 2 characters long')
-      .max(50, 'Name cannot exceed 50 characters')
-      .regex(/^[a-zA-Z\s]+$/, 'Name can only contain letters and spaces'),
-  })
-  .merge(authSchema);
+export const signInSchema = signUpSchema.omit({ name: true });
 
-export type AuthSchema = z.output<typeof authSchema>;
-export type SignUpSchema = z.output<typeof signUpSchema>;
+export type SignInSchemaType = z.output<typeof signInSchema>;
+export type SignUpSchemaType = z.output<typeof signUpSchema>;
