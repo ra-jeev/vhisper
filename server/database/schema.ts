58b3a855d9d538ccf7685ab83092a5f5db1a3d27
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-14 00:08:46 +0530

    chore: update notes table schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,43 +1,36 @@
-import crypto from 'node:crypto';
-import { sql } from 'drizzle-orm';
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import crypto from "node:crypto";
+import { sql } from "drizzle-orm";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable('users', {
-  id: text('id')
+export const users = sqliteTable("users", {
+  id: text("id")
     .primaryKey()
-    .$defaultFn(() => crypto.randomBytes(12).toString('hex')),
-  name: text('name').notNull(),
-  username: text('username').notNull().unique(),
-  password: text('password').notNull(),
-  avatar: text('avatar'),
-  createdAt: text('created_at')
+    .$defaultFn(() => crypto.randomBytes(12).toString("hex")),
+  name: text("name").notNull(),
+  username: text("username").notNull().unique(),
+  password: text("password").notNull(),
+  avatar: text("avatar"),
+  createdAt: text("created_at")
     .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`),
-  updatedAt: text('updated_at')
+  updatedAt: text("updated_at")
     .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`)
     .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
 });
 
-export const notes = sqliteTable('notes', {
-  id: text('id')
+export const notes = sqliteTable("notes", {
+  id: text("id")
     .primaryKey()
-    .$defaultFn(() => 'nt_' + crypto.randomBytes(12).toString('hex')),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
-  userId: text('user_id').references(() => users.id),
-  createdAt: text('created_at')
+    .$defaultFn(() => "nt_" + crypto.randomBytes(12).toString("hex")),
+  text: text("text").notNull(),
+  userId: text("user_id").references(() => users.id),
+  createdAt: text("created_at")
     .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`),
-  updatedAt: text('updated_at')
+  updatedAt: text("updated_at")
     .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`)
     .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
-  audioUrls: text('audio_urls', { mode: 'json' }).$type<
-    {
-      url: string;
-      duration: number;
-      recordedAt: number;
-    }[]
-  >(),
+  audioUrls: text("audio_urls", { mode: "json" }).$type<string[]>(),
 });
