566b67a0181b5da3445637247db874bd3fb1b557
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-16 20:48:44 +0530

    feat: notes display page

--- app/pages/notes/index.vue
+++ app/pages/notes/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="space-y-6">
-    <h1>This is the notes page</h1>
-    <UButton to="/notes/new">New Note</UButton>
+  <div :class="{ 'flex h-full': !notes?.length }">
+    <div v-if="notes?.length" class="space-y-4 sm:space-y-6">
+      <NoteCard v-for="note in notes" :key="note.id" :note="note" />
+    </div>
+    <div
+      v-else
+      class="flex-1 self-center text-center text-gray-500 dark:text-gray-400 space-y-2"
+    >
+      <h2 class="text-2xl md:text-3xl">No notes created</h2>
+      <p>Get started by creating your first note</p>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup lang="ts">
+const { data: notes } = await useFetch("/api/notes");
+</script>
