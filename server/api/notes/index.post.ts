ee4c490a7a6769be2e232953aadb6f6fc0161a2c
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-16 20:54:32 +0530

    feat: add /audio prefix while saving note

--- server/api/notes/index.post.ts
+++ server/api/notes/index.post.ts
@@ -2,14 +2,17 @@ import { noteSchema } from "#shared/schemas/note.schema";
 
 export default defineEventHandler(async (event) => {
   const { user } = await requireUserSession(event);
+
   const { text, audioUrls } = await readValidatedBody(event, noteSchema.parse);
 
   try {
-    await useDrizzle().insert(tables.notes).values({
-      userId: user.id,
-      text,
-      audioUrls,
-    });
+    await useDrizzle()
+      .insert(tables.notes)
+      .values({
+        userId: user.id,
+        text,
+        audioUrls: audioUrls ? audioUrls.map((url) => `/audio/${url}`) : null,
+      });
 
     return setResponseStatus(event, 201);
   } catch (err) {
