2a9d0dcf08db2635af246a210d927de65565bf80
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-16 20:45:20 +0530

    feat: RecordingsList component

---
+++ app/components/RecordingsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="space-y-2">
+    <div
+      v-for="recording in recordings"
+      :key="recording.id"
+      class="flex items-center gap-x-3 p-2 bg-gray-50 dark:bg-gray-800 rounded-lg"
+    >
+      <audio :src="recording.url" controls class="w-full h-10" />
+
+      <UTooltip text="Remove recording">
+        <UButton
+          icon="i-heroicons-trash"
+          color="red"
+          variant="ghost"
+          size="sm"
+          @click="$emit('delete', recording)"
+        />
+      </UTooltip>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ recordings: Recording[] }>();
+
+defineEmits<{
+  delete: [recording: Recording];
+}>();
+</script>
