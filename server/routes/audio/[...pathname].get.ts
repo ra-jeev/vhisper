3002bd73b760ecc3fed7c81cdea1d41b5fa0ba5b
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-16 20:55:50 +0530

    feat: /audio route for serving recordings

---
+++ server/routes/audio/[...pathname].get.ts
@@ -0,0 +1,20 @@
+export default defineEventHandler(async (event) => {
+  const { user } = await requireUserSession(event);
+
+  const { pathname } = getRouterParams(event);
+
+  try {
+    const metaData = await hubBlob().head(pathname);
+
+    if (metaData.customMetadata.userId === user.id) {
+      return hubBlob().serve(event, pathname);
+    }
+  } catch (error) {
+    console.log("failed to get the metadata", error);
+  }
+
+  throw createError({
+    statusCode: 401,
+    message: "Unauthorized!",
+  });
+});
