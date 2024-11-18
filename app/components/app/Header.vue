4a0e1934bf650fc3232bbc38555a45496da36c67
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-18 20:38:41 +0530

    refactor: app header title prop to slot

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,25 +1,17 @@
 <template>
   <header
-    class="flex justify-between items-center h-14 shrink-0 px-4 sm:px-6 border-b -mb-px sticky top-0 z-50 border-gray-200 dark:border-gray-800"
+    class="shrink-0 border-b -mb-px sticky top-0 z-50 border-gray-200 dark:border-gray-800"
   >
-    <div class="flex items-center gap-x-3">
-      <UButton
-        icon="i-heroicons-bars-3-16-solid"
-        color="gray"
-        variant="ghost"
-        class="md:hidden"
-        @click="$emit('showDrawer')"
-      />
+    <UContainer class="flex justify-between items-center h-14">
+      <div class="flex items-center gap-x-3">
+        <slot name="title">
+          <AppLogo />
+        </slot>
+      </div>
 
-      <h1 class="text-xl md:text-2xl font-semibold">{{ title }}</h1>
-    </div>
-
-    <slot name="actions" />
+      <div class="flex items-center gap-x-3">
+        <slot name="actions" />
+      </div>
+    </UContainer>
   </header>
 </template>
-
-<script setup lang="ts">
-defineProps<{ title: string }>();
-
-defineEmits(["showDrawer"]);
-</script>
