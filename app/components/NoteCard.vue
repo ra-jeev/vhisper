fd1bf230350ca590e766b8beaa1b1b6cd8ced729
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-18 23:57:59 +0530

    refactor: show time ago in note card

--- app/components/NoteCard.vue
+++ app/components/NoteCard.vue
@@ -1,29 +1,23 @@
 <template>
-  <UCard>
-    <div class="space-y-2">
-      <p
-        ref="text"
-        :class="['whitespace-pre-wrap', !showFullText && 'line-clamp-3']"
-      >
-        {{ note.text }}
-      </p>
-      <UButton
-        v-if="shouldShowExpandBtn"
-        variant="link"
-        :padded="false"
-        @click="showFullText = !showFullText"
-      >
-        {{ showFullText ? "Show less" : "Show more" }}
-      </UButton>
-
-      <p class="text-sm text-gray-500 dark:text-gray-400">
-        Created: {{ formatDate(note.createdAt) }}
-      </p>
-    </div>
+  <UCard class="hover:shadow-lg transition-shadow">
+    <p
+      ref="text"
+      :class="['whitespace-pre-wrap', !showFullText && 'line-clamp-3']"
+    >
+      {{ note.text }}
+    </p>
+    <UButton
+      v-if="shouldShowExpandBtn"
+      variant="link"
+      :padded="false"
+      @click="showFullText = !showFullText"
+    >
+      {{ showFullText ? "Show less" : "Show more" }}
+    </UButton>
 
     <div
       v-if="note.audioUrls && note.audioUrls.length > 0"
-      class="mt-6 flex gap-x-2 overflow-x-auto"
+      class="mt-4 flex gap-x-2 overflow-x-auto"
     >
       <audio
         v-for="url in note.audioUrls"
@@ -33,24 +27,22 @@
         class="w-60 shrink-0 h-10"
       />
     </div>
+
+    <p
+      class="flex items-center text-sm text-gray-500 dark:text-gray-400 gap-x-2 mt-6"
+    >
+      <UIcon name="i-heroicons-clock" class="w-4 h-4" />
+      {{ formatTimeAgo(new Date(note.createdAt + "Z")) }}
+    </p>
   </UCard>
 </template>
 
 <script setup lang="ts">
+import { formatTimeAgo } from "@vueuse/core";
+
 defineProps<{ note: Note }>();
 
 const showFullText = ref(false);
-
-const formatDate = (time: string) => {
-  const date = new Date(time);
-  return date.toLocaleString("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  });
-};
 
 const noteText = useTemplateRef<HTMLParagraphElement>("text");
 const shouldShowExpandBtn = ref(false);
