73a0bf33908301cb69411367c9f61043776e943e
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-30 13:34:01 +0530

    chore: add seo meta

--- app/app.vue
+++ app/app.vue
@@ -7,3 +7,15 @@
     </NuxtLayout>
   </UApp>
 </template>
+
+<script setup lang="ts">
+const title = "Vhisper - Voice Notes with AI Transcriptions";
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description:
+    "Write notes using your voice, and automatically transcribe the recordings, and optionally refine/post-process the transcriptions with AI.",
+  ogImage: "https://vhisper.nuxt.dev/home.png",
+  twitterCard: "summary_large_image",
+});
+</script>
